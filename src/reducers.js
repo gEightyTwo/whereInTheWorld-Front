cc7c4342de164ef85519cea584acea547225aa9e
Abe Queen <abedababe8@gmail.com>
2018-06-05 12:10:00 -0700

    receiving scores and progress bar

--- src/reducers.js
+++ src/reducers.js
@@ -1,11 +1,35 @@
 import { combineReducers } from 'redux'
+import { SET_CITY_INFO, SET_CITY_IMG, SET_CITY_SCORES } from './actions'
+
+
+const cityInfo = (state = [], action) => {
+  switch(action.type){
+    case SET_CITY_INFO:
+      return action.payload
+    default:
+      return state
+  }
+}
+const cityImages= (state = [], action) => {
+  switch(action.type){
+    case SET_CITY_IMG:
+      return action.payload
+    default:
+      return state
+  }
+}
+const cityScores = (state = [], action) => {
+  switch(action.type){
+    case SET_CITY_SCORES:
+      return action.payload
+    default:
+      return state
+  }
+}
 
 
 
 
 
 
-
-
-
-export default combineReducers({  })
+export default combineReducers({ cityInfo, cityImages, cityScores })
