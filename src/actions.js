e40328166c589996fffee77f4d9a02e96923f1ce
dmshikoff <dmshikoff@gmail.com>
2018-06-05 16:37:54 -0700

    working on city card display, route completed.

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
+import { request } from './helper'
 
 export const SET_CITY_INFO = 'SET_CURRENT_CITY'
 export const SET_CITY_IMG = 'SET_CITY_IMG'
 export const SET_CITY_SCORES = 'SET_CITY_SCORES'
+export const SET_CITY_WITH_MOST_COMMENTS = 'SET_CITY_WITH_MOST_COMMENTS'
+
 
 
 export const getCityInfo = (cityName) => {
@@ -31,4 +34,14 @@ export const getCityScores = (cityName) => {
       dispatch({type: SET_CITY_SCORES, payload: {categories: response.data.categories, summary: response.data.summary, score:response.data.teleport_city_score}})
     })
   }
+}
+
+export const getCityCard = () => {
+    return(dispatch) => {
+        request('/cities')
+        .then(response => {
+            console.log(response.data)
+            dispatch({type: SET_CITY_WITH_MOST_COMMENTS, payload: response.data.data})
+        })
+    }
 }
