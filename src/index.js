4f65b5d6e6ded2572a1d4d4b62405bcd6d24dd4e
Abe Queen <abedababe8@gmail.com>
2018-06-05 09:15:42 -0700

    route to fullcity, redux setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styling/index.css';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
